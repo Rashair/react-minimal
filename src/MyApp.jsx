cae9677177dbeccdd81ee81cc79771b0bf685c17
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-10-08 09:47:12 +0200

    Lab1Ex2

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import AppTitle from "./AppTitle";
 
+const generateArray = n => Array.from(new Array(n), (x,i) => i + 1);
+
 const MyApp = () => (
   <div>
     <AppTitle />
     <section> Lorem ipsum</section>
+    <section> {generateArray(5).toString()} </section>
   </div>
 );
 
