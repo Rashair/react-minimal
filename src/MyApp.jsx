d8ba9950592a81eeabbe9667fc8c46067431edda
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-10-09 11:47:28 +0200

    Lab1Ex4

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -8,12 +8,16 @@ const max = 25;
 const generateRandomArray = n =>
   [...new Array(n)].map(() =>  min + Math.round(Math.random() * (max - 1)));
 
+
+const constArray = [2, 56, 23, 88, 17, 4];
+
 const MyApp = () => (
   <div>
     <AppTitle />
     <section> Lorem ipsum</section>
     <section> {generateArray(5).toString()} </section>
     <section> {generateRandomArray(5).toString()} </section>
+    <section> {constArray.filter(x => x > 15).toString()}</section>
   </div>
 );
 
