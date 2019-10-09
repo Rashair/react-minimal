a3909ec979229dde817ad5e39fc2b21d18f31c20
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-10-09 11:55:03 +0200

    Lab1Ex6

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -14,6 +14,66 @@ const constArray = [2, 56, 23, 88, 17, 4];
 const constArray1 = [2, 5, 8, 10];
 const squareRoot = arr => arr.map(x => Math.sqrt(x).toFixed(3));
 
+const data = [
+  {
+    "teacherName": "Jan Nowak",
+    "teacherAge": 36,
+    "active": true,
+    "students": [
+      {
+        "name": "Maciej Janosz",
+        "age": 12
+      },
+      {
+        "name": "Wojciech Kowalski",
+        "age": 15
+      },
+      {
+        "name": "Wioletta PoznaĹska",
+        "age": 1000000
+      }
+    ]
+  },
+  {
+    "teacherName": "Mariusz Flasinski",
+    "teacherAge": 56,
+    "active": true,
+    "students": [
+      {
+        "name": "Jan Kot",
+        "age": 12
+      },
+      {
+        "name": "Jan Ziobro",
+        "age": 15
+      },
+      {
+        "name": "Adam Malysz",
+        "age": 41
+      }
+    ]
+  },
+  {
+    "teacherName": "Wojciech Kuzak",
+    "teacherAge": 44,
+    "active": false,
+    "students": [
+      {
+        "name": "Janina Wronska",
+        "age": 22
+      },
+      {
+        "name": "John Dover",
+        "age": 7
+      },
+      {
+        "name": "Emil Petterson",
+        "age": 46
+      }
+    ]
+  }
+]
+
 const MyApp = () => (
   <div>
     <AppTitle />
