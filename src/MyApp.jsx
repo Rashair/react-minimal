d1eaeb5cbfbc959f67a8495263452b9e38d19f5d
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-10-09 10:46:08 +0200

    Lab1Ex3

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -3,11 +3,17 @@ import AppTitle from "./AppTitle";
 
 const generateArray = n => Array.from(new Array(n), (x,i) => i + 1);
 
+const min = 1;
+const max = 25;
+const generateRandomArray = n =>
+  [...new Array(n)].map(() =>  min + Math.round(Math.random() * (max - 1)));
+
 const MyApp = () => (
   <div>
     <AppTitle />
     <section> Lorem ipsum</section>
     <section> {generateArray(5).toString()} </section>
+    <section> {generateRandomArray(5).toString()} </section>
   </div>
 );
 
